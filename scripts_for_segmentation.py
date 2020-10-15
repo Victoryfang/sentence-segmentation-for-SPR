224cd63d8a3ef8062e244257f091223d7d8f8436
Shaohua <44270427+Victoryfang@users.noreply.github.com>
2020-10-14 22:39:17 -0400

    Add files via upload

---
+++ scripts_for_segmentation.py
@@ -0,0 +1,13 @@
+import nltk
+
+lst=list(open('yes.txt')) # This is to import txt file
+
+for i in range(len(lst)):
+    lst[i]=lst[i].replace('\n','') # This is to remove \n
+
+s=[nltk.word_tokenize(s) for s in lst] # list comprehension
+
+y=[s.split() for s in lst]  #list comprehension
+
+for s in y:
+    print(s, len(s)) # This is to make a list of each sentence
